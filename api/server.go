9305ce99c3e9f95766dd4aeba098f5623dbb455a
Gerardo Avelar <gerardo.avelarp@gmail.com>
2023-03-09 19:28:31 -0600

    Added first API handlers with gin

---
+++ api/server.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	db "github.com/gerardoavelar/simplebank/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+// Server serves HTTP requests
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// New server instance
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts/", server.listAccount)
+
+	server.router = router
+	return server
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
