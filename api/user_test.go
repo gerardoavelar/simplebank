5824875780c32d4ad41eff494b448790dccb3dc8
Gerardo Avelar <gerardo.avelarp@gmail.com>
2023-05-29 18:58:01 -0600

    Fixed user match function

--- api/user_test.go
+++ api/user_test.go
@@ -223,5 +223,8 @@ func requireBodyMatchUser(t *testing.T, body *bytes.Buffer, user db.User) {
 	var gotUser db.User
 	err := json.NewDecoder(body).Decode(&gotUser)
 	require.NoError(t, err)
-	require.Equal(t, user, gotUser)
+	require.Equal(t, user.Username, gotUser.Username)
+	require.Equal(t, user.FullName, gotUser.FullName)
+	require.Equal(t, user.Email, gotUser.Email)
+	require.Empty(t, gotUser.HashedPassword)
 }
